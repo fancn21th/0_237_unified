866119461eeef52a18d648d54fed58631254ce3e
fancn21th <fancn21th@gmail.com>
2024-05-31 20:19:17 +0800

    📦 NEW: doc

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-import { stream } from "unified-stream";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
+// import { stream } from "unified-stream";
+// import { unified } from "unified";
+// import remarkParse from "remark-parse";
+// import remarkRehype from "remark-rehype";
+// import rehypeStringify from "rehype-stringify";
 
-const processor = unified()
-  .use(remarkParse)
-  .use(remarkRehype)
-  .use(rehypeStringify);
+// const processor = unified()
+//   .use(remarkParse)
+//   .use(remarkRehype)
+//   .use(rehypeStringify);
 
-process.stdin.pipe(stream(processor)).pipe(process.stdout);
+// process.stdin.pipe(stream(processor)).pipe(process.stdout);
+
+import fs from "fs";
+import { retext } from "retext";
+import { reporter } from "vfile-reporter";
+import retextSentenceSpacing from "./plugins/index.js";
+
+const buffer = fs.readFileSync("example.md");
+
+retext()
+  .use(retextSentenceSpacing)
+  .process(buffer)
+  .then((file) => {
+    console.error(reporter(file));
+  });
---
+++ plugins/index.js
@@ -0,0 +1,9 @@
+import { visit } from "unist-util-visit";
+
+export default function retextSentenceSpacing() {
+  return (tree, file) => {
+    visit(tree, "ParagraphNode", (node) => {
+      console.log(node);
+    });
+  };
+}
