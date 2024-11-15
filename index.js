93dfd27dfecfed59b88b098fed37511d40ad977a
Fan, Yi-Jie <fancn21th@aliyun.com>
2024-11-15 15:18:47 +0800

    📦 NEW: rehype-highlight

--- index.js
+++ index.js
@@ -1,69 +1,10 @@
-// import { stream } from "unified-stream";
-// import { unified } from "unified";
-// import remarkParse from "remark-parse";
-// import remarkRehype from "remark-rehype";
-// import rehypeStringify from "rehype-stringify";
+import {rehype} from 'rehype'
+import rehypeHighlight from 'rehype-highlight'
+import {read} from 'to-vfile'
 
-// const processor = unified()
-//   .use(remarkParse)
-//   .use(remarkRehype)
-//   .use(rehypeStringify);
+const file = await rehype()
+  .data('settings', {fragment: true})
+  .use(rehypeHighlight)
+  .process(await read('example.html'))
 
-// process.stdin.pipe(stream(processor)).pipe(process.stdout);
-
-// import fs from "fs";
-// import { retext } from "retext";
-// import { reporter } from "vfile-reporter";
-// import retextSentenceSpacing from "./plugins/index.js";
-
-// const buffer = fs.readFileSync("example.md");
-
-// retext()
-//   .use(retextSentenceSpacing)
-//   .process(buffer)
-//   .then((file) => {
-//     console.error(reporter(file));
-//   });
-
-// import { stream } from "unified-stream";
-// import { unified } from "unified";
-// import remarkParse from "remark-parse";
-// import remarkGfm from "remark-gfm";
-// import remarkRehype from "remark-rehype";
-// import rehypeStringify from "rehype-stringify";
-
-// const processor = await unified()
-//   .use(remarkParse)
-//   .use(remarkGfm)
-//   // .use(remarkRehype)
-//   .use(remarkRehype, {
-//     footnoteBackLabel(referenceIndex, rereferenceIndex) {
-//       return (
-//         "Hochspringen nach: " +
-//         (referenceIndex + 1) +
-//         (rereferenceIndex > 1 ? "-" + rereferenceIndex : "")
-//       );
-//     },
-//     footnoteLabel: "Fußnoten",
-//   })
-//   .use(rehypeStringify);
-
-// process.stdin.pipe(stream(processor)).pipe(process.stdout);
-
-import { stream } from "unified-stream";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import remarkGfm from "remark-gfm";
-import rehypeStringify from "rehype-stringify";
-import { default as plugin } from "./plugin.js";
-
-const processor = unified()
-  .use(remarkParse) // markdown -> mdast
-  .use(remarkGfm)
-  .use(plugin)
-  // .use(headingPlugin)
-  .use(remarkRehype) // mdast -> hast
-  .use(rehypeStringify); // hast -> html
-
-process.stdin.pipe(stream(processor)).pipe(process.stdout);
+console.log(String(file))
